ef03411eb10ed0d85335e25571a90f9e5533915a
Devin N. Bialo <devin.n.bialo@avanade.com>
2020-11-24 15:44:48 -0500

    Adding some new objects required for vieweing past state.

---
+++ ALifeUniv/ALife/WorldObjects/AgentShadow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+
+namespace ALifeUni.ALife
+{
+    public class EmptyObject : WorldObject
+    {
+        public EmptyObject(Point centrePoint, float startRadius, string collisionLevel) : this(centrePoint, startRadius, collisionLevel, String.Empty)
+        {
+        }
+        public EmptyObject(Point centrePoint, float startRadius, string collisionLevel, string name) : base(centrePoint, startRadius, "Empty", name, collisionLevel, Colors.Gray)
+        {
+        }
+
+        public override void Die()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExecuteAliveTurn()
+        {
+            //Do Nothing
+        }
+
+        public override void ExecuteDeadTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override WorldObject Reproduce(bool exactCopy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

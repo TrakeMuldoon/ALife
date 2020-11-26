de643a1fc7233a0a459adc5da4668b5ca66de7bc
Devin N. Bialo <devin.n.bialo@avanade.com>
2020-11-26 16:32:30 -0500

    starting AgentDistributionBehaviours

---
+++ ALifeUniv/ALife/AgentDistribution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALifeUni.ALife
+{
+    class AgentDistribution
+    {
+        public bool RandomDistribution(int numberOfAgents, Zone startingZone, bool avoidCollisions)
+        {
+            return false;
+        }
+
+        public bool StraightLineDistribution(int numberOfAgents, Zone startingZone, int Direction, int lineWidth, int agentSeparation)
+        {
+            return false;
+        }
+
+    }
+}

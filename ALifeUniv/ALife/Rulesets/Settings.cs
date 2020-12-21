ffe63d61b426af13951f0b0efbf5345a986e5859
Devin N. Bialo <devin.n.bialo@avanade.com>
2020-12-21 16:20:48 -0500

    Removed unneeded settings

--- ALifeUniv/ALife/Rulesets/Settings.cs
+++ ALifeUniv/ALife/Rulesets/Settings.cs
@@ -3,12 +3,6 @@
     public static class Settings
     {
         //TODO Extract Settings into a file somehow
-        public const float ObjectRadiusMax = 15;
-        public const float ObjectRadiusMin = 1;
-
-        public const float AgentRadiusMax = 10;
-        public const float AgentRadiusMin = 5;
-
         public const double AgentDefaultSpeed = 5;
         public const double AgentMaximumTurnDegrees = 90;
 

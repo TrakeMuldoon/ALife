fd330c4ffa2b687ce988490ca2df400b79708dce
Devin Bialo <devin.bialo@gmail.com>
2020-12-28 22:47:12 -0500

    Implmenting new Sense. GoalSense

---
+++ ALifeUniv/ALife/AgentPieces/Senses/GoalSense/GoalSenseCluster.cs
@@ -0,0 +1,34 @@
+﻿using ALifeUni.ALife.UtilityClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALifeUni.ALife.AgentPieces.Senses.GoalSense
+{
+    public class GoalSenseCluster : SenseCluster
+    {
+        public GoalSenseCluster(WorldObject parent, string name) : base(parent, name)
+        {
+        }
+
+        public override IShape Shape => throw new NotImplementedException();
+
+
+        public override void Detect()
+        {
+            base.Detect();
+        }
+
+        public override SenseCluster CloneSense(WorldObject newParent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override SenseCluster ReproduceSense(WorldObject newParent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

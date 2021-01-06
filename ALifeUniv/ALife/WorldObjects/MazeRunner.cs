759bb9c2e73598fde8f44ad6e99b4847700d8089
Devin N. Bialo <devin.n.bialo@avanade.com>
2021-01-06 13:41:13 -0500

    Fixes to the Mazerunner dude, QLearning Tutorial

--- ALifeUniv/ALife/WorldObjects/MazeRunner.cs
+++ ALifeUniv/ALife/WorldObjects/MazeRunner.cs
@@ -16,29 +16,30 @@ namespace ALifeUni.ALife.WorldObjects
     {
         public MazeRunner(Zone parentZone, Zone targetZone) : base("MazeRunner", AgentIDGenerator.GetNextAgentId(), ReferenceValues.CollisionLevelPhysical)
         {
-            this.Zone = parentZone;
-            this.TargetZone = targetZone;
+            Zone = parentZone;
+            TargetZone = targetZone;
 
             Point centrePoint = parentZone.Distributor.NextAgentCentre(10, 10); 
 
             IShape myShape = new Circle(centrePoint, 5);
-            this.StartOrientation = 0;
+            StartOrientation = 0;
             myShape.Orientation.Degrees = 0;
             myShape.Color = Colors.Red;
-            this.SetShape(myShape);
+            SetShape(myShape);
 
             List<SenseCluster> agentSenses = new List<SenseCluster>()
             {
                 new EyeCluster(this, "EyeLeft"
-                                , new ROEvoNumber(startValue: -30, evoDeltaMax: 20, hardMin:-360, hardMax: 360)   //Orientation Around Parent
+                                , new ROEvoNumber(startValue: -15, evoDeltaMax: 20, hardMin:-360, hardMax: 360)   //Orientation Around Parent
                                 , new ROEvoNumber(startValue: 10, evoDeltaMax:30, hardMin:-360, hardMax: 360)    //Relative Orientation
-                                , new ROEvoNumber(startValue: 80, evoDeltaMax:3, hardMin:40, hardMax:120)        //Radius
+                                , new ROEvoNumber(startValue: 60, evoDeltaMax:3, hardMin:40, hardMax:90)        //Radius
                                 , new ROEvoNumber(startValue: 25, evoDeltaMax:1, hardMin:15, hardMax:40)),       //Sweep
                 new EyeCluster(this, "EyeRight"
-                                , new ROEvoNumber(startValue: 30, evoDeltaMax: 20, hardMin:-360, hardMax: 360)   //Orientation Around Parent
+                                , new ROEvoNumber(startValue: 15, evoDeltaMax: 20, hardMin:-360, hardMax: 360)   //Orientation Around Parent
                                 , new ROEvoNumber(startValue: -10, evoDeltaMax:30, hardMin:-360, hardMax: 360)   //Relative Orientation
-                                , new ROEvoNumber(startValue: 80, evoDeltaMax:3, hardMin:40, hardMax:120)        //Radius
+                                , new ROEvoNumber(startValue: 60, evoDeltaMax:3, hardMin:40, hardMax:90)        //Radius
                                 , new ROEvoNumber(startValue: 25, evoDeltaMax:1, hardMin:15, hardMax:40)),       //Sweep
+                new GoalSenseCluster(this, "Goals", TargetZone)
             };
 
             List<PropertyInput> agentProperties = new List<PropertyInput>();
@@ -58,10 +59,28 @@ namespace ALifeUni.ALife.WorldObjects
 
             this.AttachAttributes(agentSenses, agentProperties, agentStatistics, agentActions);
 
-            IBrain newBrain = new BehaviourBrain(this, "IF EyeLeft.SeeSomething.Value Equals [True] THEN Rotate.TurnRight AT [0.04]"
-                                                     , "IF EyeRight.SeeSomething.Value Equals [True] THEN Rotate.TurnLeft AT [0.04]"
+            IBrain newBrain = new BehaviourBrain(this, "IF EyeLeft.SeeSomething.Value Equals [True] THEN Rotate.TurnRight AT [0.045]"
+                                                     , "IF EyeRight.SeeSomething.Value Equals [True] THEN Rotate.TurnLeft AT [0.035]"
                                                      , "IF EyeRight.SeeSomething.Value Equals [False] AND " +
-                                                          "EyeLeft.SeeSomething.Value Equals [False] THEN Move.GoForward AT [0.4]"
+                                                          "EyeLeft.SeeSomething.Value Equals [False] AND " +
+                                                          "Goals.RelativeAngle.Value GreaterThan [0] " +
+                                                          "THEN Rotate.TurnRight AT [0.1]"
+                                                     , "IF EyeRight.SeeSomething.Value Equals [False] AND " +
+                                                          "EyeLeft.SeeSomething.Value Equals [False] AND " +
+                                                          "Goals.RelativeAngle.Value LessThan [0] " +
+                                                          "THEN Move.GoForward AT [0.2]"
+                                                     , "IF EyeRight.SeeSomething.Value Equals [False] AND " +
+                                                          "EyeLeft.SeeSomething.Value Equals [False] AND " +
+                                                          "Goals.RelativeAngle.Value LessThan [0] " +
+                                                          "THEN Rotate.TurnLeft AT [0.1]"
+                                                     , "IF EyeRight.SeeSomething.Value Equals [False] AND " +
+                                                          "EyeLeft.SeeSomething.Value Equals [False] AND " +
+                                                          "Goals.RelativeAngle.Value LessThan [0] " +
+                                                          "THEN Move.GoForward AT [0.2]"
+                                                     , "IF EyeRight.SeeSomething.Value Equals [False] AND " +
+                                                          "EyeLeft.SeeSomething.Value Equals [False] AND " +
+                                                          "Goals.RelativeAngle.Value EqualTo [0] " +
+                                                          "THEN Move.GoForward AT [0.4]"
                                                      , "IF EyeRight.SeeSomething.Value Equals [True] AND " +
                                                           "EyeLeft.SeeSomething.Value Equals [True] THEN Move.GoBackward AT [1.0]"
                                                      , "IF EyeRight.SeeSomething.Value Equals [True] AND " +

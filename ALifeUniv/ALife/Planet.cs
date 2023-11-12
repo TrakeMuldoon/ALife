936101a5a5a63aa77445e30f39a42696b4fbdc2c
Christian Webber <ricky.webber@hotmail.com>
2023-11-12 16:40:44 -0500

    Your TODO was accurate from my super basic testing.

--- ALifeUniv/ALife/Planet.cs
+++ ALifeUniv/ALife/Planet.cs
@@ -156,37 +156,37 @@ namespace ALifeUni.ALife
 
         public void ExecuteOneTurn()
         {
-            turns++;
-            int order = 0;
-            //Iterate through all the active objects, and execute their turn.
-            //Note, some of them may have "died" in the meantime. They still "execute" a turn. 
-            foreach(WorldObject wo in StableActiveObjects)
-            {
-                wo.ExecutionOrder = order++;
-                wo.ExecuteTurn();
+            lock {
+                turns++;
+                int order = 0;
+                //Iterate through all the active objects, and execute their turn.
+                //Note, some of them may have "died" in the meantime. They still "execute" a turn. 
+                foreach (WorldObject wo in StableActiveObjects)
+                {
+                    wo.ExecutionOrder = order++;
+                    wo.ExecuteTurn();
+                }
+
+                //Add all the new objects into the Stable list
+                if (NewActiveObjects.Count > 0)
+                {
+                    StableActiveObjects.AddRange(NewActiveObjects);
+                    NewActiveObjects.Clear();
+                }
+                // Remove objects as necessary
+                while (ToRemoveObjects.Count > 0)
+                {
+                    AllActiveObjects.Remove(ToRemoveObjects[0]);
+                    StableActiveObjects.Remove(ToRemoveObjects[0]);
+
+                    //It needs to be added to the InactiveObjects list, for statistics reasons. 
+                    InactiveObjects.Add(ToRemoveObjects[0]);
+
+                    ToRemoveObjects.RemoveAt(0);
+                }
+
+                GlobalEndOfTurnActions();
             }
-            //Add all the new objects into the Stable list
-            //TODO: Investigate if we can add them as a range, and just clear the list after.
-            while(NewActiveObjects.Count > 0)
-            {
-                StableActiveObjects.Add(NewActiveObjects[0]);
-
-                NewActiveObjects.RemoveAt(0);
-            }
-            //TODO: Investigate this. It is probably unnecessary to remove from NewActiveObjects, as it has already been emptied.
-            while(ToRemoveObjects.Count > 0)
-            {
-                AllActiveObjects.Remove(ToRemoveObjects[0]);
-                StableActiveObjects.Remove(ToRemoveObjects[0]);
-                NewActiveObjects.Remove(ToRemoveObjects[0]);
-
-                //It needs to be added to the InactiveObjects list, for statistics reasons. 
-                InactiveObjects.Add(ToRemoveObjects[0]);
-
-                ToRemoveObjects.RemoveAt(0);
-            }
-
-            GlobalEndOfTurnActions();
         }
 
         internal void GlobalEndOfTurnActions()

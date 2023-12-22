ea8ab92f505113cba1d8c760997154d2db36c2e8
Christian Webber <ricky.webber@hotmail.com>
2023-12-21 20:29:40 -0500

    World Runner GUI Improvements, Part 1 (#26)
    * Top Bar Control separated.
    
    * More Changes
    
    * "Created" what will be the eventual controls.

---
+++ Runners/Avalonia/ALife.Avalonia/Controls/WorldRunnerAgentDetails.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ALife.Avalonia.Controls
+{
+    public partial class WorldRunnerAgentDetails : UserControl
+    {
+        public WorldRunnerAgentDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Runners/Avalonia/ALife.Avalonia/Controls/WorldRunnerWorldDetails.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ALife.Avalonia.Controls
+{
+    public partial class WorldRunnerWorldDetails : UserControl
+    {
+        public WorldRunnerWorldDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}

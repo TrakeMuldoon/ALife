2e40f9037ab979fdd0a48dbea776e14d1404bf30
Devin Bialo <devin.bialo@gmail.com>
2023-12-21 22:32:05 -0500

    REmoved duplicate renderer

--- Runners/Avalonia/ALife.Avalonia/Controls/SingularRunnerControls/WorldCanvas.cs
+++ Runners/Avalonia/ALife.Avalonia/Controls/SingularRunnerControls/WorldCanvas.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ALife.Avalonia.ALifeImplementations;
 using ALife.Avalonia.ViewModels;
 using ALife.Core;
 using ALife.Core.WorldObjects;

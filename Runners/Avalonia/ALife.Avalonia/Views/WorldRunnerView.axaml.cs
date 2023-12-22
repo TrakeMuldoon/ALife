2b0249b313fb2977439d91b6cb2b5a54add9530c
Christian Webber <ricky.webber@hotmail.com>
2023-12-21 19:30:43 -0500

    Top Bar Control separated.

--- Runners/Avalonia/ALife.Avalonia/Views/WorldRunnerView.axaml.cs
+++ Runners/Avalonia/ALife.Avalonia/Views/WorldRunnerView.axaml.cs
@@ -1,11 +1,8 @@
+using System;
 using ALife.Avalonia.ViewModels;
 using ALife.Rendering;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Avalonia.Threading;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace ALife.Avalonia.Views
 {
@@ -20,16 +17,6 @@ namespace ALife.Avalonia.Views
         private bool disposedValue;
 
         /// <summary>
-        /// The text update thread cancellation token
-        /// </summary>
-        private CancellationTokenSource _textUpdateThreadCancellationToken;
-
-        /// <summary>
-        /// The text update thread
-        /// </summary>
-        private Task _textUpdateThread;
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="WorldRunnerView"/> class.
         /// </summary>
         public WorldRunnerView()
@@ -41,56 +28,6 @@ namespace ALife.Avalonia.Views
             VisualSettingsList.Items.Clear();
             VisualSettingsList.ItemsSource = TheWorldCanvas.Simulation.Layers;
             AgentUI.DataContext = TheWorldCanvas.Simulation.AgentUiSettings;
-
-            this.TicksPerSecond.Text = ViewModel?.TicksPerSecondLabel;
-            this.FramesPerSecond.Text = ViewModel?.FramesPerSecondLabel;
-
-            _textUpdateThreadCancellationToken = new();
-            CancellationToken ct = _textUpdateThreadCancellationToken.Token;
-            _textUpdateThread = Task.Run(() => UpdateTextboxesMain(ct), ct);
-        }
-
-        /// <summary>
-        /// Updates the textboxes main.
-        /// </summary>
-        /// <param name="token">The token.</param>
-        private void UpdateTextboxesMain(CancellationToken token)
-        {
-            while (true)
-            {
-                if (token.IsCancellationRequested)
-                {
-                    token.ThrowIfCancellationRequested();
-                }
-                // With bindings, TPS updates, but for some reason FPS does _not_ update. So we're doing it manually
-                try
-                {
-                    // Start the job on the ui thread and return immediately.
-                    Dispatcher.UIThread.Post(() => UpdateTextboxes());
-
-                    // Start the job on the ui thread and wait for the result.
-                    Dispatcher.UIThread.InvokeAsync(UpdateTextboxes).Wait();
-
-                    // This invocation would cause an exception because we are
-                    // running on a worker thread:
-                    // System.InvalidOperationException: 'Call from invalid thread'
-                }
-                catch(Exception)
-                {
-                    throw; // Todo: Handle exception.
-                }
-                Thread.Sleep(500);
-            }
-        }
-
-        /// <summary>
-        /// Updates the textboxes.
-        /// </summary>
-        private void UpdateTextboxes()
-        {
-            // With bindings, TPS updates, but for some reason FPS does _not_ update. So we're doing it manually
-            this.TicksPerSecond.Text = ViewModel?.TicksPerSecondLabel;
-            this.FramesPerSecond.Text = ViewModel?.FramesPerSecondLabel;
         }
 
         /// <summary>
@@ -101,6 +38,13 @@ namespace ALife.Avalonia.Views
 
         public void _Click(object sender, RoutedEventArgs args)
         {
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
 
         /// <summary>
@@ -289,6 +233,21 @@ namespace ALife.Avalonia.Views
             Zoom_Slider.Value -= 100;
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposedValue)
+            {
+                if(disposing)
+                {
+                    // Managed items go here
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
         /// <summary>
         /// Sets the state of the simulation run.
         /// </summary>
@@ -353,37 +312,8 @@ namespace ALife.Avalonia.Views
             }
         }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if(!disposedValue)
-            {
-                if(disposing)
-                {
-                    _textUpdateThreadCancellationToken.Cancel();
-                    while (!_textUpdateThread.IsCompleted)
-                    {
-                        Thread.Sleep(100);
-                    }
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                disposedValue = true;
-            }
-        }
-
         // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~WorldRunnerView()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
+        // ~WorldRunnerView() { // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        // Dispose(disposing: false); }
     }
 }

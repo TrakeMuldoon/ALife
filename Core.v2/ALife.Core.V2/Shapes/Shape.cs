818f4fd371cc154f8505116d445a283cf28c9695
Christian Webber <ricky.webber@hotmail.com>
2024-01-04 17:08:50 -0500

    wip

---
+++ Core.v2/ALife.Core.V2/Shapes/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ALife.Core.CollisionDetection;
+using ALife.Core.Geometry;
+
+namespace ALife.Core.Shapes
+{
+    public abstract class Shape
+    {
+        public BoundingBox BoundingBox;
+
+        public Point CentrePoint;
+
+        public List<Shape> Children;
+        public Angle Orientation;
+        public Shape Parent;
+        public ShapeRenderComponents RenderComponents;
+        protected Point PreviousPoint;
+
+        public abstract Shape Clone();
+
+        public abstract void TriggerRecalculations();
+    }
+}

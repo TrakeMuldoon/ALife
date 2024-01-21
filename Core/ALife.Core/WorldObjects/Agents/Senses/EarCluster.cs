68f85e58c87c68e09b523f7dbcbb273b86653b30
Devin Bialo <devin.bialo@gmail.com>
2024-01-21 13:47:29 -0500

    Added Sound Support Ojbects.

---
+++ Core/ALife.Core/WorldObjects/Agents/Senses/EarCluster.cs
@@ -0,0 +1,66 @@
+﻿using ALife.Core.Geometry;
+using ALife.Core.Geometry.Shapes;
+using ALife.Core.Geometry.Shapes.ChildShapes;
+using ALife.Core.Utility;
+using ALife.Core.WorldObjects.Agents.Senses.GenericInputs;
+using System;
+using System.Collections.Generic;
+
+namespace ALife.Core.WorldObjects.Agents.Senses
+{
+    public class EarCluster : SenseCluster
+    {
+        ChildCircle myShape;
+        public override IShape Shape
+        {
+            get { return myShape; }
+        }
+
+        private EvoNumber EvoOrientationAroundParent;
+        private EvoNumber EvoRadius;
+
+        public EarCluster(WorldObject parent, String name
+                          , EvoNumber eOrientationAroundParent, EvoNumber eRadius)
+            : base(parent, name)
+        {
+            EvoOrientationAroundParent = eOrientationAroundParent;
+            EvoRadius = eRadius;
+
+            Angle orientationAroundParent = new Angle(eOrientationAroundParent.StartValue);
+            float radius = (float)eRadius.StartValue;
+            myShape = new ChildCircle(parent.Shape, orientationAroundParent
+                                      , 5.0 //TODO: Similar to EyeCluster Issue. This is a hack.
+                                      , radius);
+            SubInputs.Add(new AnyInput(name + ".HearSomething"));
+            //SubInputs.Add(new )
+        }
+
+        public override SenseCluster CloneSense(WorldObject newParent)
+        {
+            EarCluster newEC = new EarCluster(newParent, Name
+                                              , EvoOrientationAroundParent.Clone(), EvoRadius.Clone());
+            return newEC;
+        }
+
+        public override SenseCluster ReproduceSense(WorldObject newParent)
+        {
+            EarCluster newEC = new EarCluster(newParent, Name
+                                              , EvoOrientationAroundParent.Evolve(), EvoRadius.Evolve());
+            return newEC;
+        }
+
+        public Dictionary<string, string> ExportEvoNumbersAsCode()
+        {
+            Dictionary<string, string> properties = new Dictionary<string, string>();
+            PopulateCodeDictionary(properties, "OrientationAroundParent", EvoOrientationAroundParent);
+            PopulateCodeDictionary(properties, "Radius", EvoRadius);
+
+            return properties;
+        }
+
+        private void PopulateCodeDictionary(Dictionary<string, string> properties, string name, EvoNumber evo)
+        {
+            properties.Add(name, $", new ROEvoNumber(startValue: {evo.StartValue},\tevoDeltaMax: {evo.DeltaMax},\thardMin: {evo.ValueHardMin},\thardMax: {evo.ValueHardMax})\t//{name}");
+        }
+    }
+}
---
+++ Core/ALife.Core/WorldObjects/SoundWave.cs
@@ -0,0 +1,47 @@
+﻿using ALife.Core.Geometry.Shapes;
+using System;
+
+namespace ALife.Core.WorldObjects
+{
+    public class SoundWave : WorldObject
+    {
+        static int SoundUniqueID = 1;
+
+        public byte Intensity;
+        public byte Pitch;
+        public byte Timbre;
+
+        public SoundWave(byte intensity, byte pitch, byte timbre, Point coords)
+            : base(coords, new Circle(coords, intensity), "Sound", (SoundUniqueID++).ToString(), ReferenceValues.CollisionLevelSound, System.Drawing.Color.WhiteSmoke)
+        {
+            Intensity = intensity;
+            Pitch = pitch;
+            Timbre = timbre;
+        }
+
+        public override void Die()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExecuteAliveTurn()
+        {
+            TrashItem();
+        }
+
+        public override void ExecuteDeadTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override WorldObject Clone()
+        {
+            throw new NotImplementedException("SoundWaves should not be cloned");
+        }
+
+        public override WorldObject Reproduce()
+        {
+            throw new NotImplementedException("SoundWaves should not reproduce");
+        }
+    }
+}

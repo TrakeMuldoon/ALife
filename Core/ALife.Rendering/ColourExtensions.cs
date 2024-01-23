1be9211ea931039dd1d2d4170c864de734bb3c91
Christian Webber <ricky.webber@hotmail.com>
2024-01-23 14:57:15 -0500

    Breaking all the things!

---
+++ Core/ALife.Rendering/ColourExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using ALife.Core.Utility.Colours;
+
+namespace ALife.Rendering
+{
+    /// <summary>
+    /// Extensions to Colours.
+    /// </summary>
+    public class ColourExtensions
+    {
+        /// <summary>
+        /// Converts an IColour to a System.Drawing.Color.
+        /// </summary>
+        /// <param name="colour">The colour.</param>
+        /// <returns>The Color.</returns>
+        public static Color ToColor(IColour colour)
+        {
+            return Color.FromArgb(colour.A, colour.R, colour.G, colour.B);
+        }
+    }
+}

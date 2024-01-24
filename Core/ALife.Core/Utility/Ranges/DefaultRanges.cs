e0521f3fecc6db85adf5c9e75606595e098942b8
Christian Webber <ricky.webber@hotmail.com>
2024-01-23 20:21:44 -0500

    Extra comments.

--- Core/ALife.Core/Utility/Ranges/DefaultRanges.cs
+++ Core/ALife.Core/Utility/Ranges/DefaultRanges.cs
@@ -1,21 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ALife.Core.Utility.Ranges
+﻿namespace ALife.Core.Utility.Ranges
 {
+    /// <summary>
+    /// Various default-defined ranges for use in the ALife project.
+    /// </summary>
     public static class DefaultRanges
     {
+        /// <summary>
+        /// A range representing the full range of a double.
+        /// </summary>
         public static readonly Range<double> FullDoubleRange = new Range<double>(double.MinValue, double.MaxValue);
 
+        /// <summary>
+        /// A range representing a percentage (0/0% -> 1/100%).
+        /// </summary>
         public static readonly Range<double> DoublePercentageRange = new Range<double>(0, 1);
 
+        /// <summary>
+        /// A range representing the valid numbers for a neural network.
+        /// </summary>
+        public static readonly Range<double> NeuralNetworkRange = new Range<double>(-1, 1);
+
+        /// <summary>
+        /// A range representing the full range of a byte.
+        /// </summary>
         public static readonly Range<byte> FullByteRange = new Range<byte>(byte.MinValue, byte.MaxValue);
 
+        /// <summary>
+        /// A range representing the default range we generally want for random colours.
+        /// </summary>
         public static readonly Range<byte> RandomColourRange = new Range<byte>(100, 255);
 
+        /// <summary>
+        /// A range representing the degrees in a circle.
+        /// </summary>
         public static readonly Range<int> DegreeRange = new Range<int>(0, 360);
     }
 }
